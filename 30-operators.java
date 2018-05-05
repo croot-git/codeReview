886380ff12f63db9ccd31898449ae7c1cae901a5
denodo1 <denodo1@gmail.com>
2018-05-05 12:38:19 +0000

    Update 30-operators.java

--- 30-operators.java
+++ 30-operators.java
@@ -11,7 +11,7 @@ public class Solution {
         double meal_cost = in.nextDouble();
         int tip_percent = in.nextInt();
         int tax_percent = in.nextInt();
-        double tip = Math.round(meal_cost * (tip_percent * 0.01) * 10d)/10d;
+        double tip = meal_cost * (tip_percent * 0.01);
         double tax = meal_cost * (tax_percent * 0.01);
         //System.out.println(meal_cost + ", " + tip + ", " + tax);
         System.out.println("The total meal cost is " + Math.round(meal_cost + tip + tax) + " dollars.");
