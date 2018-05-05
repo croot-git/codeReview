a19c7872913ab5dc4a2445980b1e190cc9a4358d
denodo1 <denodo1@gmail.com>
2018-05-05 13:13:35 +0000

    Update 30-data-types.java

--- 30-data-types.java
+++ 30-data-types.java
@@ -5,21 +5,13 @@ public class Solution {
         String s = "HackerRank ";
 		
         Scanner scan = new Scanner(System.in);
-        /* Declare second integer, double, and String variables. */
-        /* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
         
         int j = scan.nextInt();
         double e = scan.nextDouble();
         String v = scan.nextLine();
-        
-        /* Print the sum of both integer variables on a new line. */
-        /* Print the sum of the double variables on a new line. */
-        /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
         	
         System.out.println(i + j);
-		System.out.println(d + e);
+        System.out.println(d + e);
         System.out.println(s + v);
         
         scan.close();
