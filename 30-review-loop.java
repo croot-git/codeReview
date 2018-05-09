0b0aa6f401029452a3794d4dd6be3a7aed68d18b
denodo1 <denodo1@gmail.com>
2018-05-09 09:04:39 +0000

    Add new file

---
+++ 30-review-loop.java
@@ -0,0 +1,25 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+        String s = "";
+        String oddRs = "";
+        String evenRs = "";
+        sc.nextLine();
+		for (int i = 0; i < T; i++) {
+            s = sc.nextLine();
+            String[] sArr = s.split("");
+            for(int j = 0 ; j < sArr.length ; j++) {
+                if(j%2==0)
+                    evenRs += sArr[j];
+                else
+                    oddRs += sArr[j];
+            }
+            System.out.println(evenRs + " " + oddRs);
+            oddRs = "";
+            evenRs = "";
+        }
+    }
+    
+}
