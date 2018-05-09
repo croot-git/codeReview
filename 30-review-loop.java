80438369c178f16bd70e59481d43a261349f1e40
denodo1 <denodo1@gmail.com>
2018-05-09 09:05:21 +0000

    Update 30-review-loop.java

--- 30-review-loop.java
+++ 30-review-loop.java
@@ -2,7 +2,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+	int T = sc.nextInt();
         String s = "";
         String oddRs = "";
         String evenRs = "";
