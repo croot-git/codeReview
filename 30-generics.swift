99f00f501ff8f86608c568ebe4e36401b1c95cf5
croot <forever90051@gmail.com>
2018-06-14 05:13:15 +0000

    Day 21: Generics

---
+++ 30-generics.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+struct Printer<T> {
+	/**
+	*    Name: printArray
+	*    Print each element of the generic array on a new line. Do not return anything.
+	*    @param A generic array
+	**/
+    
+    func printArray(array: [T]) {
+        for item in array {
+            print(item)
+        }
+    }
+}
+
+var n = Int(readLine()!)!
+var intArray = Array(repeating: 0, count: n);
+for i in 0...n - 1 {
+	intArray[i] = Int(readLine()!)!;
+}
+
+n = Int(readLine()!)!
+var stringArray = Array(repeating: "", count: n);
+for i in 0...n - 1 {
+	stringArray[i] = readLine()!;
+}
+
+Printer<Int>().printArray(array: intArray)
+Printer<String>().printArray(array: stringArray)
